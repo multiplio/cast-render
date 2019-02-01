c47e7103d9ba81ae68d71080200aa6906d0c2385
WhoMeNope <whomenope@outlook.com>
2019-02-01 22:08:55 +0100

    push fix

--- push.sh
+++ push.sh
@@ -7,4 +7,4 @@ gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 docker tag ${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:latest gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT
 gcloud docker push gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}
 
-yes | gcloud beta container images add-tag gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:latest
+gcloud container images add-tag gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:latest
