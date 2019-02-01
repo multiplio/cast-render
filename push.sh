226885633573949f4083934e26e0d42544938124
WhoMeNope <whomenope@outlook.com>
2019-02-01 22:30:19 +0100

    gcloud auth docker

--- push.sh
+++ push.sh
@@ -2,9 +2,10 @@
 set -e
 
 echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
-gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+gcloud --quiet auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+gcloud --quiet auth configure-docker
 
 docker tag ${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:latest gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT
-gcloud docker push gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}
+docker push gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}
 
-gcloud container images add-tag gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:latest
+gcloud --quiet container images add-tag gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:latest
